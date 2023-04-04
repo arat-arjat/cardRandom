83046aa3651c3f42dbf7ca4673c629baa7c491a1
arat-arjat <anderaniel@gmail.com>
2023-04-04 22:07:26 +0000

    proyecto culminado

--- src/app.js
+++ src/app.js
@@ -4,8 +4,50 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { read } from "@popperjs/core";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let numerosLetras = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let Palos = ["♦", "♥", "♠", "♣"];
+
+  let numerosLetrasRandom =
+    numerosLetras[Math.floor(Math.random() * numerosLetras.length)];
+
+  let PalosRandom = Palos[Math.floor(Math.random() * Palos.length)];
+
+  let RandomResul = PalosRandom;
+
+  let valor = document.getElementsByClassName("center");
+  valor[0].textContent = numerosLetrasRandom;
+
+  let palosLetf = document.getElementsByClassName("left");
+  palosLetf[0].textContent = RandomResul;
+
+  let palosRight = document.getElementsByClassName("right");
+  palosRight[0].textContent = RandomResul;
+
+  if (PalosRandom == "♥" || PalosRandom == "♦") {
+    palosLetf[0].style.color = "red";
+    palosRight[0].style.color = "red";
+  } else {
+    palosLetf[0].style.color = "black";
+    palosRight[0].style.color = "black";
+  }
 };
